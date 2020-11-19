49a2401cd8e7b9998b53f578c0d015b643112c13
denizhankara <denizhankara@ku.edu.tr>
2020-11-19 08:12:41 +0300

    parsed timeseries with specific scenarios

--- parser_with_timestamps.py
+++ parser_with_timestamps.py
@@ -6,8 +6,11 @@ from parse import parse
 from glob import glob
 from typing import List, Dict
 from progressbar import ProgressBar
+import pandas as pd
+import tsfresh
 
-PATH_TO_SCEN = "/ul/ul_vertsys/ul_wqy57/" # path to scenario root
+
+PATH_TO_SCEN = "work/ul/ul_vertsys/ul_wqy57/" # path to scenario root
 PATH_TO_JSON = "/veins-maat/simulations/securecomm2018/results/"
 SCEN_PREFIX = "veins_maat.uc1." # the prefix of the scenario
 INDEX_TRIGGER = "| --- | --- | --- | --- | --- |"
@@ -63,106 +66,133 @@ def feature_extraction_singh(json_file:str, extraction_type: int, malicious_vehi
             exit("Undefined singh extraction type: %d"%(extraction_type))
     return output
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='This script parses VeReMi dataset')
-    parser.add_argument('--dataset_root', type=str, required=True, help='The root directory of the dataset. The directory should be named \"work/\"')
-    parser.add_argument('--index', type=str, required=True, help="The path to index.md as specified in the VeReMi github dataset")
-    parser.add_argument('--feature_extraction', type=str, default='singh', help="The path to index.md as specified in the VeReMi github dataset")
-    parser.add_argument('--output', type=str, default='./parsed/', help="The directory of the parsed output saved as .npy")
-    parser.add_argument('--overwrite', type=int, default=0, help="0 := Do not overwrite preprocessed data, 1 := Overwite preprocessed data")
-    parser.add_argument('--save_as_numpy', type=bool, default=False, help="Flag to save the parsed data as numpy")
-    ARGS = parser.parse_args()
+def vehicleParser(json_file,path_to_json):
+
+
+    log_name_tokens = os.path.splitext(json_file)[0].split("-")
+    vehicle_id = log_name_tokens[1] # [0]:= 'JSONlog' [1]:=vehicle instance count [2]:=vehicle id [1]:=vehicle attack type
+    vehicle_OMNET_ID = log_name_tokens[2] # [0]:= 'JSONlog' [1]:=vehicle instance count [2]:=vehicle id [1]:=vehicle attack type
     
-    # Check if the dataset directory is correct
-    dataset_scen_root =  ARGS.dataset_root + PATH_TO_SCEN
-    print(dataset_scen_root)
-    if not os.path.basename(ARGS.dataset_root) == "work" or not os.path.isdir(dataset_scen_root):
-        exit("The specified path %s is not the correct dataset root"%(ARGS.dataset_root))
+    attack_type = log_name_tokens[3]
+    if attack_type == 'A0':
+        is_vehicle_attacker = 0
+    else:
+        is_vehicle_attacker= 1 
 
-    # Check if the index file is correct
-    index_content = []
-    index_trigger_flag = False
-    if not os.path.isfile(ARGS.index):
-        exit("The specified index file does not exist")
-    if not os.path.isdir(ARGS.output):
-        os.mkdir(ARGS.output)
+    # start parsing the file
+    vehicleList= []
+    file_path = path_to_json+json_file
+    with open(file_path, "r") as f:
+        log_rows = f.readlines()
 
-    with open(ARGS.index, "r") as f:
-        for l in f.readlines():
-            if not index_trigger_flag and INDEX_TRIGGER not in l:
-                continue
-            elif index_trigger_flag:
-                index_content.append(l.strip())
-            elif INDEX_TRIGGER in l:
-                index_trigger_flag = True
-    # Build 2 dicts
-    ATTR_TO_DIRNAME = {}
-    DIRNAME_TO_ATTR = {}
-    for row in index_content:
-        # Check format
-        rep, density_level, attacker_type, attacker_density, dirname, _ = parse(ROW_FORMAT, row)
-        rep = int(rep)
-        density_level = int(density_level)
-        attacker_type = int(attacker_type)
-        attacker_density = float(attacker_density)
-        dirname = parse("{}.tgz", dirname)[0]
-        ATTR_TO_DIRNAME[hash_attr(rep,density_level, attacker_type,attacker_density)] = dirname
-        DIRNAME_TO_ATTR[dirname] = {"rep":rep,"density_level":density_level,"attacker_type":attacker_type,"attacker_density":attacker_density}
-    with open(ARGS.output + "index.json",'w') as f:
-        json.dump(DIRNAME_TO_ATTR,f,indent=4)
+    for row in log_rows:    
+        json_row = json.loads(row)
+        json_row['vehicle_ID'] = vehicle_id # set here, change later
+        json_row['vehicle_OMNET_ID'] = vehicle_OMNET_ID
+        json_row['is_attacker'] = is_vehicle_attacker
+        if json_row['type'] == 2 :
+            pass
+        else:
+            
+            # extract position 
+            json_row['pos_x'] = json_row['pos'][0] 
+            json_row['pos_y'] = json_row['pos'][1] 
+            json_row['pos_z'] = json_row['pos'][2] 
+            del json_row['pos']
+            # extract speed 
+            json_row['spd_x'] = json_row['spd'][0] 
+            json_row['spd_y'] = json_row['spd'][1] 
+            json_row['spd_z'] = json_row['spd'][2] 
+            del json_row['spd']
+            
+            # extract position noise
+            json_row['pos_x_noise'] = json_row['pos_noise'][0] 
+            json_row['pos_y_noise'] = json_row['pos_noise'][1] 
+            json_row['pos_z_noise'] = json_row['pos_noise'][2] 
+            del json_row['pos_noise']
 
-    # Get Scenario Names and parse the data content
-    DIRNAME_TO_DATA={}
-    total_scenarios = len(glob(dataset_scen_root+SCEN_PREFIX+"*"))
-    pbar = ProgressBar()
-    scen_count = 0
-    for scen_full_path in pbar(glob(dataset_scen_root+SCEN_PREFIX+"*")):
-        scen_count += 1
-        scen_name = os.path.basename(scen_full_path)
-        if scen_name not in DIRNAME_TO_ATTR:
-            exit("Unexpected directory encountered: %s"%(scen_full_path))
-        if not os.path.isdir(scen_full_path+PATH_TO_JSON):
-            exit("The directory %s does not have path to json data: expected dir %s"%(scen_full_path, scen_full_path+PATH_TO_JSON))
+            # extract spd noise 
+            json_row['spd_x_noise'] = json_row['spd_noise'][0] 
+            json_row['spd_y_noise'] = json_row['spd_noise'][1] 
+            json_row['spd_z_noise'] = json_row['spd_noise'][2] 
+            del json_row['spd_noise']
 
-        # Check if the file already exists. If it does, check if the overwite is set
-        parsed_scen_output_dir = os.path.join(ARGS.output, scen_name + ".json")
-        if os.path.isfile(parsed_scen_output_dir) and ARGS.overwrite == 0:            
-            continue
+            # add new dict to list
+            vehicleList.append(json_row)
+            
+            
+    current_df = pd.DataFrame(vehicleList)
+            
+    # returns a dataframe
 
-        # Check for json files
-        ground_truth_dir = scen_full_path+PATH_TO_JSON+"GroundTruthJSONlog.json"
-        if not os.path.isfile(ground_truth_dir):
-            exit("Expected GroundTruthJSONlog.json at dir %s but does not exist"%(scen_full_path+PATH_TO_JSON))
+    return current_df
+    
 
-        vehicles_logs = {}
-        attacker_id = set()
-        ground_truth = []
-        ground_truth_index_by_id = {}
-        with open(ground_truth_dir, "r") as f:
-            ground_truth_rows = f.readlines()
-        for row in ground_truth_rows:
-            msg = json.loads(row)
-            if msg['attackerType'] != 0:
-                attacker_id.add(msg['sender'])
-            ground_truth.append(msg)
-            ground_truth_index_by_id[msg["messageID"]] = msg
-        assert len(ground_truth) == len(ground_truth_index_by_id)
+def parseAndExport(jsondir,exportfilename): # currently manual name, will be automated eventually
 
-        # Get files names of each vehicle logs
-        for d in glob(scen_full_path+PATH_TO_JSON+"*.json"):
-            if d == ground_truth_dir:
-                continue
-            log_name_tokens = os.path.splitext(os.path.basename(d))[0].split("-")
-            JSONlog = log_name_tokens[1] # [0]:= 'JSONlog' [1]:=vehicle instance count [2]:=vehicle id [1]:=vehicle attack type
-            vehicle_id = log_name_tokens[2] # [0]:= 'JSONlog' [1]:=vehicle instance count [2]:=vehicle id [1]:=vehicle attack type
-            vlog = feature_extraction_singh(d, 3, attacker_id)
-            vehicles_logs[vehicle_id]= vlog
-        vehicles_logs['attr'] = DIRNAME_TO_ATTR[scen_name]
-        with open(parsed_scen_output_dir, 'w') as f:
-            if not ARGS.save_as_numpy:
-                json.dump(vehicles_logs, f) 
-            else:
-                np.save(f, vehicles_logs)
-    print(scen_count)
-    print(len(DIRNAME_TO_ATTR))
-    assert scen_count == len(DIRNAME_TO_ATTR.keys())
+    big_df = pd.DataFrame()
+    json_files = [pos_json for pos_json in os.listdir(path_to_json) if (pos_json.endswith('.json') and pos_json.startswith('JSONlog'))]
+    for json_file in json_files :
+        parsed_df = vehicleParser(json_file,path_to_json) #.sort_values(["sender","rcvTime"])
+        if not parsed_df.empty:
+            parsed_df = parsed_df.sort_values(["sender","rcvTime"])
+        big_df = pd.concat([big_df,parsed_df], axis=0, ignore_index=True)
+
+    
+    big_df.to_csv(index=False,path_or_buf=exportfilename)
+
+    # Done parsing
+    print("Done")
+    return big_df
+    
+
+if __name__ == "__main__":
+    
+    print("Start parsing as time series")
+    # get the files list 
+    # big_df = pd.DataFrame()
+    #path_to_JSON_folder= "work/ul/ul_vertsys/ul_wqy57/veins_maat.uc1.14506023.180205_200518/veins-maat/simulations/securecomm2018/results/JSONlog-0-7-A0.json"
+    
+    #exit()
+    # get all the JSON files from directory
+    
+    #Automate later 
+    path_to_json = 'work/ul/ul_vertsys/ul_wqy57/veins_maat.uc1.14505247.180205_165727/veins-maat/simulations/securecomm2018/results/'
+    parseAndExport(path_to_json,"veins_maat.uc1.14505247.180205_165727.csv")
+    
+    path_to_json = 'work 2/ul/ul_vertsys/ul_wqy57/veins_maat.uc1.14505342.180205_171010/veins-maat/simulations/securecomm2018/results/'
+    parseAndExport(path_to_json,"veins_maat.uc1.14505342.180205_171010.csv")
+    
+    path_to_json = 'work 3/ul/ul_vertsys/ul_wqy57/veins_maat.uc1.14505511.180205_173553/veins-maat/simulations/securecomm2018/results/'
+    parseAndExport(path_to_json,"veins_maat.uc1.14505511.180205_173553.csv")
+    
+    path_to_json = 'work 4/ul/ul_vertsys/ul_wqy57/veins_maat.uc1.14505930.180205_192941/veins-maat/simulations/securecomm2018/results/'
+    parseAndExport(path_to_json,"veins_maat.uc1.14505930.180205_192941.csv")
+    
+    path_to_json = 'work 5/ul/ul_vertsys/ul_wqy57/veins_maat.uc1.14506016.180205_200240/veins-maat/simulations/securecomm2018/results/'
+    parseAndExport(path_to_json,"veins_maat.uc1.14506016.180205_200240.csv")
+    
+    print("All done")
+
+
+
+
+    
+    """
+    json_files = [pos_json for pos_json in os.listdir(path_to_json) if (pos_json.endswith('.json') and pos_json.startswith('JSONlog'))]
+    for json_file in json_files :
+        parsed_df = vehicleParser(json_file,path_to_json) #.sort_values(["sender","rcvTime"])
+        if not parsed_df.empty:
+            parsed_df = parsed_df.sort_values(["sender","rcvTime"])
+        big_df = pd.concat([big_df,parsed_df], axis=0, ignore_index=True)
+
+    #big_df.to_csv(index=False,path_or_buf='export.csv')
+    
+    # Do feature extraction and selection
+
+    #big_df = big_df.sort_values(["sender","rcvTime","vehicle_ID"])
+    big_df.to_csv(index=False,path_or_buf='export.csv')
+
+    # Done parsing
+    print("Done")
+    """
